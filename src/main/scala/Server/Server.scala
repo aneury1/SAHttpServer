fce036d816b5a2edad994f7c773732b482ea9269
aneury1 <aneury1@gmail.com>
2022-05-16 11:46:31 -0400

    Adding default respose

--- src/main/scala/Server/Server.scala
+++ src/main/scala/Server/Server.scala
@@ -4,12 +4,23 @@ class AhttpServer(port : Int = 8080) {
     var server: ServerSocket = null
     var handler:RequestHandler = null
     type RequestHandler = (Socket) => Unit
-    def default_response_server(s: Socket )={
 
+    def default_response_server(s: Socket )={
+        var bytes = new Array[Byte](4096)
+        var input = s.getInputStream()
+        var output = s.getOutputStream()
+        var read = input.read(bytes)
+        var response = "HTTP/1.1 200 OK\r\n" +
+            "Content-Type: text/html\r\n" +
+            "Content-Length: " + read + "\r\n" +
+            "\r\n" +
+            new String(bytes, 0, read)
+        output.write(response.getBytes())
     }
 
     def createServer(requestHandler: RequestHandler= default_response_server)={
         server = new ServerSocket(port)
+        this.handler = requestHandler
         if(server==null){
             println("Server is not created")
         }else{
@@ -25,6 +36,7 @@ class AhttpServer(port : Int = 8080) {
             this.handler(client)
         }else{
             println("No handler is set")
+            default_response_server(client)
             client.close()
         }
     }
